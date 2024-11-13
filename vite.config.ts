e7f3b89dd6828a7e5a7461e2fbc8dd1ae2cd668a
valts08 <valts.pilans@gmail.com>
2024-11-13 20:42:17 +0200

    trying to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/expense_tracker/',
-  plugins: [react()],
+  plugins: [react(), ghPages()],
 })
